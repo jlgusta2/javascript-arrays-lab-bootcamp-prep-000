bd16ff37f76c4bf345fee0f2edae4de50b56aea5
jennifer gustavson <jengustavson@gmail.com>
2018-05-23 21:19:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+  
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens;
+}
+
+function appendKitten(name){
+  var newKitty = [...kittens]
+  newKitty.push(name)
+  return newKitty;
+}
+
+function prependKitten(name){
+  var newKitty = [...kittens]
+  newKitty.unshift(name)
+  return newKitty;
+}
+
+function removeLastKitten(){
+  var newKitty = [...kittens]
+  newKitty.pop()
+  return newKitty;
+}
+
+function removeFirstKitten(){
+  var newKitty = [...kittens]
+  newKitty.shift()
+  return newKitty;
+  
+}
+
